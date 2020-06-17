cf5782fdb91842bdc4c7d6d8afdf68cf7fe677b6
kentcheung2000 <kentcheung2000@yahoo.com>
2020-06-17 13:54:01 -0700

    56. Icon widget

--- lib/details_screen.dart
+++ lib/details_screen.dart
@@ -14,7 +14,7 @@ class _DetailsScreenState extends State<DetailsScreen> {
     final hotel = ModalRoute.of(context).settings.arguments as Hotel;
     return Scaffold(
       appBar: AppBar(
-        title: Text('Details'),
+        title: Text(hotel.name),
       ),
       body: Column(
         children: <Widget>[
@@ -27,10 +27,73 @@ class _DetailsScreenState extends State<DetailsScreen> {
             ),
           ),
           Flexible(
-              flex: 3,
-              child: Container(
-                color: Colors.black26,
-              ))
+            flex: 3,
+            child: Container(
+              padding: EdgeInsets.all(10),
+              color: Colors.black26,
+              child: Column(
+                children: <Widget>[
+                  Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15),
+                      color: Colors.white,
+                    ),
+                    padding: EdgeInsets.all(10),
+                    child: Row(
+                      children: <Widget>[
+                        Text('Rating: '),
+                        hotel.rating >= 1
+                            ? Icon(
+                                Icons.star,
+                                color: Colors.blue,
+                              )
+                            : Icon(
+                                Icons.star_border,
+                                color: Colors.blue,
+                              ),
+                        hotel.rating >= 2
+                            ? Icon(
+                                Icons.star,
+                                color: Colors.blue,
+                              )
+                            : Icon(
+                                Icons.star_border,
+                                color: Colors.blue,
+                              ),
+                        hotel.rating >= 3
+                            ? Icon(
+                                Icons.star,
+                                color: Colors.blue,
+                              )
+                            : Icon(
+                                Icons.star_border,
+                                color: Colors.blue,
+                              ),
+                        hotel.rating >= 4
+                            ? Icon(
+                                Icons.star,
+                                color: Colors.blue,
+                              )
+                            : Icon(
+                                Icons.star_border,
+                                color: Colors.blue,
+                              ),
+                        hotel.rating >= 5
+                            ? Icon(
+                                Icons.star,
+                                color: Colors.blue,
+                              )
+                            : Icon(
+                                Icons.star_border,
+                                color: Colors.blue,
+                              ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+          )
         ],
       ),
     );
